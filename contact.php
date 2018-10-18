68c18fe92b4d520b661b08f202a9da8757c9642c
sendibel <44243141+sendibel@users.noreply.github.com>
2018-10-18 13:08:02 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,137 @@
+<?php 
+require('include/header.php');
+
+?>
+</head>
+<body style="background-color: #129BFF;"><img src="/records2/images/voda.gif" style="width: 100%;padding-bottom: 30px;">
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" style="height: 92px;">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        	<ul class="navbar-nav mr-auto">
+        		<li class="nav-item active">
+        			<a class="nav-link" href="<?php echo 'home.php' ?>">HOME <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo 'contact.php' ?>">CONTACT <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo 'records.php' ?>">RECORDS <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main role="main">
+    	<div class="jumbotron">
+    		<div class="container">
+    			<h3 class="display-1">Records!</h1>
+        		<p>The record is the highest achievement of sportsmen or teams in a sports branch, achieved in a particular area or in a sports organization, and in accordance with the conditions for achieving the record by sports organizations in their rules of competition. Records are registered in those sports branches that can provide unique conditions for achieving: unique rules, uniform battlefields, standardized sports devices and the method of measuring the success of athletes.</p>
+        		<div class="gif" style="float: right;">
+        			<img src="/records2/images/dama.gif">
+        		</div>
+        		<p><a class="btn btn-primary btn-lg" href="<?php echo 'info1.php' ?>" role="button">Learn more »</a></p>
+        	</div>
+        </div>
+        <div class="container">
+        	<div class="row">
+        		<div class="col-md-4">
+        			<h2>The first official records</h2>
+        			<p></p><br><hr style="color: white;">
+        			<p>The first official records began to register with the establishment of international sports federations. That's right:
+		            The International Skating Union (ISU) was founded in 1892, and since that year, the World Record (SR) has been running in a fast slip.
+		            The International Amateur Athletic Federation (IAAF) was founded in 1912, but as the first SR registered results in running at 100 and 400 m (Tom Berk USA 12.0 sec or 54.2 sec), and running at 800 and 1500 m Edwin Tedi Flek Australia, 2.11.0 min and 4.33.2 min) achieved at the Olympic Games in 1896.The International Swimming Federation FINA was founded in 1908 and since then the SR has been in swimming.</p><hr>
+		            <p><a class="btn btn-secondary" href="<?php echo 'myplace.php' ?>" role="button">about my favoryte place »</a></p>
+		            <div>
+		            	<img src="/records2/images/plivanje1.gif" style="width: 350px;height: 330px;"></div>
+		            </div>
+		            <div class="col-md-4">
+		            	<h2>Separately for men and women</h2>
+		            	<hr>
+		            	<p>Sports records are seen separately for men and women. Today, officially registers record in athletics, automotive, cycling, fast skating, weightlifting, sailing, horse sport, bowling, modeling, motorcycling, motor flying, parachuting, swimming,archery and shooting. In some sports or sports disciplines where, due to the technical and atmospheric conditions of the competition, it is difficult to prescribe the unique standards for achieving the record, only the best results in the world, the region, the country (rowing, ski jumps and flights, running marathons (from 1 January 2004, world records are taking place).</p>
+		            <hr>
+		            <p><a class="btn btn-secondary" href="<?php echo 'map.php' ?>" role="button">View map »</a></p><br>
+		            <style>
+		            img {
+		              max-width: 100%;
+		              height: auto;
+		            }
+		            </style>
+		            <img src="/records2/images/separately.jpg" style="width: 350px;height: 330px;">
+		        </div>
+		        <div class="col-md-4">
+		            <h2>FINA </h2>
+		            <p></p><br><hr>
+		            <p>The records of the world swimming championships are the best results of swimmers in individual swimming disciplines, with world swimming competitions ratified by the FINA (Fédération Internationale de Natation). The record is made especially for world championships in large (long) or Olympic pools (50 m) and small (short) pools (25 m) in men's and women's competitions.Records of world championships in large pools are held from the first World Swimming Championships in Belgrade in 1973, when they competed in 29 disciplines (15 men and 14 women). In time, some disciplines were added, and today they compete in 40 disciplines of 20 in both categories.</p>
+		            <hr>
+		            <p><a class="btn btn-secondary" href="<?php echo 'records.php' ?>" role="button">View tables2 »</a></p>
+		            <div class="gif" style="float: right;">
+		            	<img src="/records2/images/fina3.jpg" style="width:350px; height:330px;">
+		            </div>
+		        </div>
+		    </div>
+		    <hr style="color: white;">
+		</div> <!-- /container -->
+	</main>
+	<footer style="padding-left: 44%;">
+		<p>Copyright2018©Swimweb</p>
+    </footer>
+
+		    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+  
+
+
