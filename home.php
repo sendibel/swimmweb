614e2bb93cdf1c3e1e05bb55d11449f52546e544
sendibel <44243141+sendibel@users.noreply.github.com>
2018-11-18 18:13:27 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,135 @@
+<?php 
+require('include/header.php');
+?>
+
+<link rel="stylesheet" type="text/css" href="css/learn.css">
+</head>
+<body style="background-color: black; width: 100%;>
+<div class="container">
+  <nav>
+    <a class="navbar-brand" href="#"><strong><i>HOME </i></strong></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation"style="border:groove;">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarsExample07">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo 'contact.php' ?>"><strong><i>CONTACT </i></strong><span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo 'records.php' ?>"><strong><i>RECORDS </i></strong><span class="sr-only">(current)</span></a>
+        </li>
+        <div style="float: right;">
+  </nav>
+  <main role="main">
+    <div class="slic">
+      <style>
+      img:hover {
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
+      }
+      </style>
+      <img src="/records2/images/akcione.jpg" style="width: 100%;border-radius: 5px;">
+    </div>
+    <div class="jumbotron"  style="background-color: #129BFF;">
+      <div class="col-sm-8 mx-auto">
+        <div>
+          <div class="ifr" style="float: right;">
+            <iframe class="embed-responsive-item" width="250" height="105" src="https://www.youtube.com/embed/sBIuJ7_Y2T4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        </div>
+
+        <button onclick="typeWriter()">Click me</button>
+        <h1 id="demo">
+
+          <script>
+          var i = 0;
+          var txt = 'About swimmers...';
+          var speed = 50;
+
+          function typeWriter() {
+            if (i < txt.length) {
+              document.getElementById("demo").innerHTML += txt.charAt(i);
+              i++;
+              setTimeout(typeWriter, speed);
+            }
+          }
+          </script>
+        </h1>
+
+        <article>
+          <span><strong>1. Elite swimmers do not allow others to slow them down in achievingstrong></span>
+          <p style="font-size: larger;color: white;">- We think it is very bad if you tellhis goal is out of reach, that he is not within his abilities or out of the scope oSome people do this because of jealousy or to justify their failures. Know thaaccept this information with the heart and can be difficult for them to talk to,may accept such information as an additional "jet fuel" that will provide them wiimpetus and motivation for success. Do not let anyone move you to your path to succdo not be the one who will challenge others.</p>
+        </article>
+        <article>
+          <span><strong>2. Do not just be strong in the story, live the life of an elite athlspan>
+          <p style="font-size: larger;color: white;">- Each of us wants to someday become an - the Olympian. Everyday conversation about this is certainly good and can be meach of us. However, successful successful swimmers do not want to just talk about it, but they put words into action and move into action. You can say that you want to become a champion, but let your words always follow your works, try to think and act in accordance with the principles of elite athletes for 24 hours.</p>
+        </article>
+   
+          <span><strong>3. Being an elite athlete is not just a habit you will show whcompany or during a part of a while</strong></span>
+          <p style="font-size: larger;color: white;">- Realize that you can not be brilliant only during parElite swimmers are as such and lie as such. Regardless of whether they are at home or within a favorite society, they always think, behave, feed, etc. in exactly the same way.</p>
+        </article>
+        <article>
+          <span><strong>4. Getting experience is what makes us elite</strong></span>
+          <p style="font-size: larger;color: white;">- Each day is a new day in which we can learn something new. Setting goals and tracking them is more important than the results, times or placements themselves. The final results are indications of what a swimmer becomes regularly going to training, listening to his trainer, tracking his weak points and investing daily work towards achieving the goals set. There will be days when you will be the first, there will be those when you do not all agree to the dice, but at the end of the day, the most important thing is how you have learned and learned from everything, because only in this way can you be successful.</p>
+        </article>
+        <article>
+          <span><strong>5. Most successful elite swimmers have very strong personal integrity</strong></span>
+          <p style="font-size: larger;color: white;">- How many times have you found yourself in a situation to commit to doing something in one day, and within a few days or days, you realize that your resolve is terminated because you feel it is too difficult or you are convinced that you can not do it. Know that elite swimmers are the toughest of themselves and that they hold the word and promises firmly, not only to others but also to themselves, because this is the most important thing.</p>
+        </article>
+        <article>
+          <span><strong>6. Accept the defeats as something completely normal</strong></span>
+          <p style="font-size: larger;color: white;">- It would be ideal if the life we ​​live in was "honey and milk". Things are unfortunately unfortunately not going according to the plan we set up and according to our wishes all the time. Every day there will be a certain obstacle in front of us, which may be smaller or bigger, it is up to us to decide whether we will do our best to advance it and win or we will simply give up. Know that there is no elite swimmer who immediately became easy with ease. Behind every elite swimmer are miles of training, pools of sweat and the hills of the nerves that they had to invest in to become what they are today. Remember this.</p>
+        </article>
+        <article>
+          <span><strong>7. Do not be afraid of planning and be realistic</strong></span>
+          <p style="font-size: larger;color: white;">- Creating success is not just an accidental set of circumstances that you expect to happen. Success represents a careful planning result in combination with focusing on action. Elite swimmers always know how to get to the goal, but they always have a "map" showing them the way in front of them. Make detailed plans, talk to your trainer every day, be persistent and you will reach your goal.</p>
+        </article>
+        <article>
+          <span><strong>8. Look for a solution, not an excuse</strong></span>
+          <p style="font-size: larger;color: white;">- And while many swimmers talk of muscle aches, homework, which they can not do or how very bad they feel about fatigue, your friends swimmers from the neighborhood will, besides the same problems, tear their sleeves and find ways to become successful and beat all kinds of challenges.</p>
+        </article>
+        <article>
+          <span><strong>9. Try to be in the surroundings of people who think lik</span>
+          <p style="font-size: larger;color: white;">- Whether we want it or not, the environment around us can have a big impact on ourselves. If we hang out with people who are more successful or trying to be, the conversation itself will be motivating enough for us to think in that direction.</p>
+        </article>
+        <article>
+          <span><strong>10. Be proactive</strong></span>
+          <p style="font-size: larger;color: white;">- Elite swimmers take action even before they need it. They do not imagine that some things will happen to them, they go out into the field, get into the pool, and do everything they need to make things and goals come true. They do not hope for success that will only come one day, but actively work to make sure it really happens.</p>
+        </article>
+  
+          <span><strong>11. Get out of the set circle and do something more for yourself</strong></span>
+          <p style="font-size: larger;color: white;">- Most swimmers will come to their training, do what they need, and go home. Elite swimmers will remain to talk about their technique with the trainer after training, to remain stretched or to perform a particular exercise if needed. The term "first entry and last exits" is certainly applicable to elite swimmers.</p>
+        </article>
+        <article>
+          <span><strong>12. Set more standards</strong></span>
+          <p style="font-size: larger;color: white;">- Good enough for elite swimmers is not good enough. Elite swimmers know that there is a possibility for further improvement and improvement. If you want to be the best you have to get to the stage where other swimmers will learn from you because you have made a certain technique or curl up to perfection. All you need is that you really want it and that you are constantly working on it.</p>
+        </article>
+        <article>
+          <span><strong>13. Be prepared to always receive a constructive critique</strong></span>
+          <p style="font-size: larger;color: white;">- Any feedback from your trainer must be considered and thought of them. The coach is one who knows your positive and negative sides and will do everything to make you a great swimmer. Elite swimmers always strive to more objectively accept and evaluate criticism towards themselves and to diligently work on them to correct them.</p>
+        </article>
+        <article>
+          <span><strong>14. Leave your pride at home</strong></span>
+          <p style="font-size: larger;color: white;">- If you want to be an elite swimmer, but also want to do just what is acceptable to you, we have bad news for you - you will not succeed in doing so. Elite swimmers are proud of the fact that they have to work hard and are willing to do whatever they do not want or are hard for them to be the best in it.</p>
+        </article>
+        <article>
+          <span><strong>15. Support for team members</strong></span>
+          <p style="font-size: larger;color: white;">- Regardless of whether you are cheering for your teammates or helping younger categories, elite swimmers have a strong sense of compassion for their colleagues and sports in general and are always ready to give support and help.</p>
+        </article>
+        <article>
+          <span><strong>16. Avoid complaints and complaints</strong></span>
+          <p style="font-size: larger;color: white;">- Elite swimmers know that putting them out will not lead to a goal, and certainly will not justify themselves as well as the bad day they had. Complaints create an ineffective and negative state of mind.</p>
+        </article>
+        <article>
+          <span><strong>17. Do not wait tomorrow, go more than today</strong></span>
+          <p style="font-size: larger;color: white;">- You must know the saying "Monday from Monday," "Starting from tomorrow," etc. With elite swimmers tomorrow is already late, they are moving now and now. Tomorrow can only be a new moment to postpone the action. We recommend that you leave right now.</p>
+        </article>
+
+        <div class="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2830.334470617501!2d20.480218815487447!3d44.81475027909873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a7abdf957d04d%3A0x571f59c54aca9a70!2z0KXQsNC70LAg0J_QuNC-0L3QuNGA!5e0!3m2!1ssr!2srs!4v1530370702205" width="600" height="450" frameborder="0" style="width: 100%;height: 60%;" allowfullscreen></iframe>
+        </div>
+      </main>
+      <footer style="padding-left: 44%;">
+        <p style="color: white;">Copyright2018©Swimweb</p>
+      </footer>
+                    
