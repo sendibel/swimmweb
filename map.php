a3f4dd51110278bee1d865b1ce7d56f2796b8a76
sendibel <44243141+sendibel@users.noreply.github.com>
2018-11-18 18:15:04 +0100

    Add files via upload

---
+++ map.php
@@ -0,0 +1,23 @@
+<?php
+require('include/header.php'); 
+?>
+  <script type="text/javascript" src="/records2/js/googlemap.js"></script>
+  <meta http-equiv="x-ua-compatible" content="IE=edge">
+  <link rel="stylesheet" type="text/css" href="/records2/css/map.css">
+</head>
+<body>
+  <div class="container caca">
+    <center><h4>access google maps api</h4></center>
+      <div id="map">
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDncW3-Wn0VQAPsWMqlk2lwveIJXsOhIog&callback=LoadMap"></script>
+      </div>
+  </div>
+</body>
+<div class="video_1">
+  <footer>
+    <p>Copyright2018©Swimweb</p>
+  </footer>
+      
+
+      
+      
