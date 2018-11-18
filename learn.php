46356af68a3a3e67d0d1cb4f426537916a48067d
sendibel <44243141+sendibel@users.noreply.github.com>
2018-11-18 18:15:56 +0100

    Add files via upload

---
+++ learn.php
@@ -0,0 +1,32 @@
+<?php 
+  require('include/header.php');
+  // require('include/footer.php');
+?>
+<!-- 
+<link rel="stylesheet" type="text/css" href="/records/assets/css/learn.css"> -->
+</head>
+  <body style="background-color: #B51622;">
+    <script>
+    $(document).ready(function(){
+        $('[data-toggle="popover"]').popover(); 
+    });
+    </script>
+    <div class="learn">
+      <div class="d_1">
+        <div class="row_1">
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+              <img src="/records/assets/images/michael.jpg" alt="michael" style="width: 200px;
+              height: 200px;">
+              <div class="caption">
+                <a href="#" data-toggle="popover" title="Phelps" data-content="Michael Phelps American swimmer was born on June 30, 1985. in Baltimore. In his previous career, he won twenty-two Olympic medals and thus became the most victorious Olympian in the history of the Olympics. Of this, a total of eighteen gold medals he won at the 2004 and 2012 Olympics. in Athens, Beijing and London. He won two silver medals in London on 200 m butterfly and 4x100 m freestyle. He won two bronze medals in Athens 200 m and 4 x 100 m freestyle. At the World Swimming Championships, he won a total of twenty-six gold medals.">About Michael Phelps
+                </a>
+                <iframe width="260" height="200" src="https://www.youtube.com/embed/e3VsKheoeBE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+                </iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+            
